5286262523081a58e2781931055e7f7c61e20225
cwowhappy <lixiaoyi1061220211@126.com>
2017-11-09 10:32:07 +0800

    做题

--- dp/m-problem-474.py
+++ dp/m-problem-474.py
@@ -6,10 +6,47 @@ class Solution:
         :type n: int
         :rtype: int
         """
+        m_n_of_strings = Solution.count_m_n_of_strings(strs)
+        print(m_n_of_strings)
+        max_numbers = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
+        return Solution.recursive1(m_n_of_strings, len(strs) - 1, m, n)
 
     @staticmethod
-    def recursive(strings, l, m, n, max_numbers):
-        if not max_numbers[l][m][n]:
-            max_numbers[l][m][n] = 0
+    def count_m_n_of_strings(strings):
+        m_n_of_strings = []
+        for string in strings:
+            m_n_of_strings.append(Solution.count_m_n_of_string(string))
+        return m_n_of_strings
 
-        return max_numbers[l][m][n]
+    @staticmethod
+    def count_m_n_of_string(string):
+        return [string.count("0"), string.count("1")]
+
+    @staticmethod
+    def recursive1(m_n_of_strings, l, m, n):
+        if l < 0:
+            return 0
+        if m >= m_n_of_strings[l][0] and n >= m_n_of_strings[l][1]:
+            return max(Solution.recursive1(m_n_of_strings, l - 1, m, n),
+                       Solution.recursive1(m_n_of_strings, l - 1, m - m_n_of_strings[l][0], n - m_n_of_strings[l][1]) + 1)
+        else:
+            return Solution.recursive1(m_n_of_strings, l - 1, m, n)
+
+    @staticmethod
+    def recursive(m_n_of_strings, l, m, n, max_numbers):
+        if l < 0:
+            return 0
+        if not max_numbers[m][n]:
+            if m >= m_n_of_strings[l][0] and n >= m_n_of_strings[l][1]:
+                max_numbers[m][n] = max(Solution.recursive(m_n_of_strings, l - 1, m, n, max_numbers),
+                                        Solution.recursive(m_n_of_strings, l - 1, m - m_n_of_strings[l][0],
+                                                           n - m_n_of_strings[l][1], max_numbers) + 1)
+            else:
+                max_numbers[m][n] = Solution.recursive(m_n_of_strings, l - 1, m, n, max_numbers)
+
+        return max_numbers[m][n]
+
+
+Array = ["1", "0", "1", "0"]
+solution = Solution()
+print(solution.findMaxForm(Array, 2, 2))
